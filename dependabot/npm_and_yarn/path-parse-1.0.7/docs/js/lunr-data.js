1bb98051019c1c8f7f0b0db967af3b3850a22598
Travis CI <travis@travis-ci.org>
2021-08-12 20:36:59 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: 44c0c009da46950a5579949b6bc5f66d3d073dea

---
+++ dependabot/npm_and_yarn/path-parse-1.0.7/docs/js/lunr-data.js
@@ -0,0 +1,550 @@
+window.lunrData = {
+  "index": {
+    "version": "1.0.0",
+    "fields": [
+      {
+        "name": "longname",
+        "boost": 1000
+      },
+      {
+        "name": "name",
+        "boost": 500
+      },
+      {
+        "name": "tags",
+        "boost": 300
+      },
+      {
+        "name": "kind",
+        "boost": 110
+      },
+      {
+        "name": "title",
+        "boost": 100
+      },
+      {
+        "name": "summary",
+        "boost": 70
+      },
+      {
+        "name": "description",
+        "boost": 50
+      },
+      {
+        "name": "body",
+        "boost": 1
+      }
+    ],
+    "ref": "id",
+    "tokenizer": "default",
+    "documentStore": {
+      "store": {
+        "index.html": [
+          "around",
+          "easi",
+          "index",
+          "jlv",
+          "json",
+          "log",
+          "readm",
+          "usag",
+          "viewer",
+          "wrapper"
+        ],
+        "global.html": [
+          "document",
+          "global"
+        ],
+        "list_module.html": [
+          "document",
+          "list",
+          "list:modul",
+          "modul"
+        ],
+        "module-helper.html": [
+          "helper",
+          "modul",
+          "module:help"
+        ],
+        "module-cli.html": [
+          "cli",
+          "modul",
+          "module:cli"
+        ]
+      },
+      "length": 5
+    },
+    "tokenStore": {
+      "root": {
+        "docs": {},
+        "a": {
+          "docs": {},
+          "r": {
+            "docs": {},
+            "o": {
+              "docs": {},
+              "u": {
+                "docs": {},
+                "n": {
+                  "docs": {},
+                  "d": {
+                    "docs": {
+                      "index.html": {
+                        "ref": "index.html",
+                        "tf": 10
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "e": {
+          "docs": {},
+          "a": {
+            "docs": {},
+            "s": {
+              "docs": {},
+              "i": {
+                "docs": {
+                  "index.html": {
+                    "ref": "index.html",
+                    "tf": 10
+                  }
+                }
+              }
+            }
+          }
+        },
+        "i": {
+          "docs": {},
+          "n": {
+            "docs": {},
+            "d": {
+              "docs": {},
+              "e": {
+                "docs": {},
+                "x": {
+                  "docs": {
+                    "index.html": {
+                      "ref": "index.html",
+                      "tf": 1300
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "j": {
+          "docs": {},
+          "l": {
+            "docs": {},
+            "v": {
+              "docs": {
+                "index.html": {
+                  "ref": "index.html",
+                  "tf": 600
+                }
+              }
+            }
+          },
+          "s": {
+            "docs": {},
+            "o": {
+              "docs": {},
+              "n": {
+                "docs": {
+                  "index.html": {
+                    "ref": "index.html",
+                    "tf": 10
+                  }
+                }
+              }
+            }
+          }
+        },
+        "l": {
+          "docs": {},
+          "o": {
+            "docs": {},
+            "g": {
+              "docs": {
+                "index.html": {
+                  "ref": "index.html",
+                  "tf": 10
+                }
+              }
+            }
+          },
+          "i": {
+            "docs": {},
+            "s": {
+              "docs": {},
+              "t": {
+                "docs": {
+                  "list_module.html": {
+                    "ref": "list_module.html",
+                    "tf": 110
+                  }
+                },
+                ":": {
+                  "docs": {},
+                  "m": {
+                    "docs": {},
+                    "o": {
+                      "docs": {},
+                      "d": {
+                        "docs": {},
+                        "u": {
+                          "docs": {},
+                          "l": {
+                            "docs": {
+                              "list_module.html": {
+                                "ref": "list_module.html",
+                                "tf": 1300
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "r": {
+          "docs": {},
+          "e": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "d": {
+                "docs": {},
+                "m": {
+                  "docs": {
+                    "index.html": {
+                      "ref": "index.html",
+                      "tf": 110
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "u": {
+          "docs": {},
+          "s": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "g": {
+                "docs": {
+                  "index.html": {
+                    "ref": "index.html",
+                    "tf": 10
+                  }
+                }
+              }
+            }
+          }
+        },
+        "v": {
+          "docs": {},
+          "i": {
+            "docs": {},
+            "e": {
+              "docs": {},
+              "w": {
+                "docs": {},
+                "e": {
+                  "docs": {},
+                  "r": {
+                    "docs": {
+                      "index.html": {
+                        "ref": "index.html",
+                        "tf": 10
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "w": {
+          "docs": {},
+          "r": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "p": {
+                "docs": {},
+                "p": {
+                  "docs": {},
+                  "e": {
+                    "docs": {},
+                    "r": {
+                      "docs": {
+                        "index.html": {
+                          "ref": "index.html",
+                          "tf": 10
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "d": {
+          "docs": {},
+          "o": {
+            "docs": {},
+            "c": {
+              "docs": {},
+              "u": {
+                "docs": {},
+                "m": {
+                  "docs": {},
+                  "e": {
+                    "docs": {},
+                    "n": {
+                      "docs": {},
+                      "t": {
+                        "docs": {
+                          "global.html": {
+                            "ref": "global.html",
+                            "tf": 35
+                          },
+                          "list_module.html": {
+                            "ref": "list_module.html",
+                            "tf": 35
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "g": {
+          "docs": {},
+          "l": {
+            "docs": {},
+            "o": {
+              "docs": {},
+              "b": {
+                "docs": {},
+                "a": {
+                  "docs": {},
+                  "l": {
+                    "docs": {
+                      "global.html": {
+                        "ref": "global.html",
+                        "tf": 2045
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "m": {
+          "docs": {},
+          "o": {
+            "docs": {},
+            "d": {
+              "docs": {},
+              "u": {
+                "docs": {},
+                "l": {
+                  "docs": {
+                    "list_module.html": {
+                      "ref": "list_module.html",
+                      "tf": 635
+                    },
+                    "module-helper.html": {
+                      "ref": "module-helper.html",
+                      "tf": 110
+                    },
+                    "module-cli.html": {
+                      "ref": "module-cli.html",
+                      "tf": 110
+                    }
+                  },
+                  "e": {
+                    "docs": {},
+                    ":": {
+                      "docs": {},
+                      "h": {
+                        "docs": {},
+                        "e": {
+                          "docs": {},
+                          "l": {
+                            "docs": {},
+                            "p": {
+                              "docs": {
+                                "module-helper.html": {
+                                  "ref": "module-helper.html",
+                                  "tf": 1300
+                                }
+                              }
+                            }
+                          }
+                        }
+                      },
+                      "c": {
+                        "docs": {},
+                        "l": {
+                          "docs": {},
+                          "i": {
+                            "docs": {
+                              "module-cli.html": {
+                                "ref": "module-cli.html",
+                                "tf": 1300
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "h": {
+          "docs": {},
+          "e": {
+            "docs": {},
+            "l": {
+              "docs": {},
+              "p": {
+                "docs": {},
+                "e": {
+                  "docs": {},
+                  "r": {
+                    "docs": {
+                      "module-helper.html": {
+                        "ref": "module-helper.html",
+                        "tf": 600
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "c": {
+          "docs": {},
+          "l": {
+            "docs": {},
+            "i": {
+              "docs": {
+                "module-cli.html": {
+                  "ref": "module-cli.html",
+                  "tf": 600
+                }
+              }
+            }
+          }
+        }
+      },
+      "length": 22
+    },
+    "corpusTokens": [
+      "around",
+      "cli",
+      "document",
+      "easi",
+      "global",
+      "helper",
+      "index",
+      "jlv",
+      "json",
+      "list",
+      "list:modul",
+      "log",
+      "modul",
+      "module:cli",
+      "module:help",
+      "readm",
+      "usag",
+      "viewer",
+      "wrapper"
+    ],
+    "pipeline": [
+      "trimmer",
+      "stopWordFilter",
+      "stemmer"
+    ]
+  },
+  "store": {
+    "index.html": {
+      "id": "index.html",
+      "kind": "readme",
+      "title": "JLV",
+      "longname": "index",
+      "name": "JLV",
+      "tags": "index",
+      "summary": "wrapper around json-log-viewer for easy usage",
+      "description": "",
+      "body": ""
+    },
+    "global.html": {
+      "id": "global.html",
+      "kind": "global",
+      "title": "Globals",
+      "longname": "global",
+      "name": "Globals",
+      "tags": "global",
+      "summary": "All documented globals.",
+      "description": "",
+      "body": ""
+    },
+    "list_module.html": {
+      "id": "list_module.html",
+      "kind": "list",
+      "title": "Modules",
+      "longname": "list:module",
+      "name": "Modules",
+      "tags": "list:module",
+      "summary": "All documented modules.",
+      "description": "",
+      "body": ""
+    },
+    "module-helper.html": {
+      "id": "module-helper.html",
+      "kind": "module",
+      "title": "helper",
+      "longname": "module:helper",
+      "name": "helper",
+      "tags": "module:helper",
+      "summary": "",
+      "description": "",
+      "body": ""
+    },
+    "module-cli.html": {
+      "id": "module-cli.html",
+      "kind": "module",
+      "title": "cli",
+      "longname": "module:cli",
+      "name": "cli",
+      "tags": "module:cli",
+      "summary": "",
+      "description": "",
+      "body": ""
+    }
+  }
+};
